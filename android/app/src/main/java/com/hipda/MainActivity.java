2786785fc5b388efedb6204787f9134840a44e76
gaoyadong <maltoze@qq.com>
2020-12-07 20:54:06 +0800

    chore: react-native-screens config

--- android/app/src/main/java/com/hipda/MainActivity.java
+++ android/app/src/main/java/com/hipda/MainActivity.java
@@ -1,5 +1,6 @@
 package com.hipda;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
@@ -17,11 +18,16 @@ public class MainActivity extends ReactActivity {
   }
 
   @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
+
+  @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new ReactActivityDelegate(this, getMainComponentName()) {
       @Override
       protected ReactRootView createRootView() {
-       return new RNGestureHandlerEnabledRootView(MainActivity.this);
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
       }
     };
   }
