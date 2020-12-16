fb295e3ffa04d179ee64ff7996e89b46b47a532f
maltoze <maltoze@qq.com>
2020-12-16 17:53:47 +0800

    feat: navigate throttle

--- src/navigation/navigate.ts
+++ src/navigation/navigate.ts
@@ -1,11 +1,22 @@
 import { StackNavigationProp } from '@react-navigation/stack';
+import throttle from 'lodash/throttle';
+
+const navigateOneTime = (navigate: any) =>
+  throttle(navigate, 1000, { trailing: false });
 
 const openProfile = (navigation: StackNavigationProp<any>) => (props = {}) => {
   navigation.push('Profile', props);
 };
 
+const openPostNextPage = (navigation: StackNavigationProp<any>) => (
+  props = {},
+) => {
+  navigation.replace('ThreadDetail', props);
+};
+
 const navigate = (navigation: any) => ({
-  openProfile: openProfile(navigation),
+  openProfile: navigateOneTime(openProfile(navigation)),
+  openPostNextPage: navigateOneTime(openPostNextPage(navigation)),
 });
 
 export default navigate;
