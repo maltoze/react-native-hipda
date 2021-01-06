4fa0b8dc2ec288bc0abfa26608132cdb8ac924b9
maltoze <maltoze@qq.com>
2021-01-06 21:05:25 +0800

    style: remove divider

--- src/components/Thread/ThreadListFooter.tsx
+++ src/components/Thread/ThreadListFooter.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { ActivityIndicator, Text, useTheme } from 'react-native-paper';
-import HiDivider from '../HiDivider';
 
 type ThreadListFooterProps = {
   refreshing: boolean;
@@ -11,16 +10,13 @@ const ThreadListFooter = (props: ThreadListFooterProps) => {
   const { refreshing } = props;
   const { colors } = useTheme();
   return (
-    <View>
-      <HiDivider />
-      <View style={[styles.container, { backgroundColor: colors.surface }]}>
-        {refreshing ? null : (
-          <>
-            <ActivityIndicator />
-            <Text style={styles.loading}>正在加载...</Text>
-          </>
-        )}
-      </View>
+    <View style={[styles.container, { backgroundColor: colors.surface }]}>
+      {refreshing ? null : (
+        <>
+          <ActivityIndicator />
+          <Text style={styles.loading}>正在加载...</Text>
+        </>
+      )}
     </View>
   );
 };
