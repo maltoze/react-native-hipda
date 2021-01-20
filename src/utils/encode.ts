095e61b8fbfebbff4e15fb550355a3fc7556f30c
maltoze <maltoze@qq.com>
2021-01-20 19:45:36 +0800

    fix: encode add padStart

--- src/utils/encode.ts
+++ src/utils/encode.ts
@@ -2,7 +2,7 @@ import iconv from 'iconv-lite';
 
 export const urlencode = (data: string, encoding = 'gbk') => {
   return [...iconv.encode(data, encoding)]
-    .map((n) => `%${n.toString(16)}`)
+    .map((n) => `%${n.toString(16).padStart(2, '0')}`)
     .join('')
     .toUpperCase();
 };
