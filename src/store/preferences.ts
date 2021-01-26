64570ca2a968ec8e3ff6e3df05ce2a86f7d48a9f
maltoze <maltoze@qq.com>
2021-01-26 15:56:59 +0800

    feat: preferences persist

--- src/store/preferences.ts
+++ src/store/preferences.ts
@@ -1,17 +1,22 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Appearance, ColorSchemeName } from 'react-native';
 import create from 'zustand';
+import { persist } from 'zustand/middleware';
 
 type State = {
   colorScheme: ColorSchemeName;
   setColorScheme: (scheme: ColorSchemeName) => void;
 };
 
-const useStore = create<State>((set) => {
-  return {
-    colorScheme: Appearance.getColorScheme(),
-    setColorScheme: (colorScheme) => set(() => ({ colorScheme })),
-  };
-});
+const useStore = create<State>(
+  persist(
+    (set) => ({
+      colorScheme: Appearance.getColorScheme(),
+      setColorScheme: (colorScheme) => set(() => ({ colorScheme })),
+    }),
+    { name: 'preferences-storage', getStorage: () => AsyncStorage },
+  ),
+);
 
 const colorScheme = (s: State) => s.colorScheme;
 const setColorScheme = (s: State) => s.setColorScheme;
